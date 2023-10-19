e54f15708a2dfc8e940f212de4b4e096f6114c37
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-20 01:28:19 +0200

    Борьба с ошибками

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,99 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        int pos = 0;
+        List<char> chars = new List<char>() { 'w', 'l', 'o', 'h', 'd', 'r' };
         public MainWindow()
         {
             InitializeComponent();
+            CreateButtons();
+
+
+        }
+        private void CreateButtons()
+        {
+            StackPanel stackPanel = new StackPanel();
+            stackPanel.Orientation = Orientation.Horizontal;
+            stackPanel.HorizontalAlignment = HorizontalAlignment.Stretch;
+            for (int i = 0; i < chars.Count; i++)
+            {
+                var b = new Button() { Content = chars[i], Width = 50, Height = 50 };
+                b.Style = (Style)FindResource("MaterialDesignPaperButton");
+                stackPanel.Children.Add(b);
+            }
+            SPBase.Children.Add(stackPanel);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tbText_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            char sign;
+            string key = e.Key.ToString();
+            if (key == "Space")
+            {
+                sign = ' ';
+            }
+            else
+            {
+                sign = char.ToLower(key[0]);
+            }
+
+            if (sign == tbText.Text.First())
+            {
+                foreach (var item in SPBase.Children)
+                {
+                    if (item is StackPanel)
+                    {
+                        foreach (var item2 in (item as StackPanel).Children)
+                        {
+                            if (item2 is Button)
+                            {
+                                if ((item2 as Button).Content.ToString() == char.ToLower(key[0]).ToString())
+                                {
+                                    (item2 as Button).Background = new SolidColorBrush(Colors.LemonChiffon);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                StringBuilder stringBuilder = new StringBuilder(tbText.Text);
+                stringBuilder.Remove(0, 1);
+                tbText.Text = stringBuilder.ToString();
+                tbText.UpdateLayout();
+            }
+
+        }
+
+
+
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            foreach (var item in SPBase.Children)
+            {
+                if (item is StackPanel)
+                {
+                    foreach (var item2 in (item as StackPanel).Children)
+                    {
+                        if (item2 is Button)
+                        {
+                            if ((item2 as Button).Content.ToString() == char.ToLower(e.Key.ToString()[0]).ToString())
+                            {
+                                (item2 as Button).Background = new SolidColorBrush(Colors.White);
+                                (item2 as Button).UpdateLayout();
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
