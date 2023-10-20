e2d74b9056f52a6fcae076eb754613fc74a18561
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-20 23:37:48 +0200

    1,3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,11 +12,12 @@ namespace WpfApp
 {
     public partial class MainWindow : Window
     {
-        int pos = 0;
         List<char> chars = new List<char>() { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', };
         List<char> chars2 = new List<char>() { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'' };
         List<char> chars3 = new List<char>() { 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/' };
         List<char> chars4 = new List<char>() { '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=' };
+
+        List<Button> buttons = new List<Button>();
 
         public MainWindow()
         {
@@ -24,6 +26,35 @@ namespace WpfApp
             CreateButtons(SPBase2, chars2);
             CreateButtons(SPBase3, chars3);
             CreateButtons(SPBase4, chars4);
+
+            foreach (var item in SPBase.Children)
+            {
+                if (item is Button button)
+                {
+                    buttons.Add(button);
+                }
+            }
+            foreach (var item in SPBase2.Children)
+            {
+                if (item is Button button)
+                {
+                    buttons.Add(button);
+                }
+            }
+            foreach (var item in SPBase3.Children)
+            {
+                if (item is Button button)
+                {
+                    buttons.Add(button);
+                }
+            }
+            foreach (var item in SPBase4.Children)
+            {
+                if (item is Button button)
+                {
+                    buttons.Add(button);
+                }
+            }
         }
 
         private void CreateButtons(StackPanel stackPanel, List<char> charsList)
@@ -32,11 +63,12 @@ namespace WpfApp
             stackPanel.HorizontalAlignment = HorizontalAlignment.Stretch;
             for (int i = 0; i < charsList.Count; i++)
             {
-                var b = new Button() { Content = charsList[i], Width = 50, Height = 50 };
+                var b = new Button() { Content = charsList[i].ToString(), Width = 50, Height = 50 };
                 b.Style = (Style)FindResource("MaterialDesignPaperButton");
                 stackPanel.Children.Add(b);
             }
         }
+
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
@@ -57,7 +89,7 @@ namespace WpfApp
 
             if (sign == tbText.Text.First())
             {
-                HighlightButton(SPBase, key);
+                HighlightButton(sign.ToString());
                 StringBuilder stringBuilder = new StringBuilder(tbText.Text);
                 stringBuilder.Remove(0, 1);
                 tbText.Text = stringBuilder.ToString();
@@ -65,21 +97,24 @@ namespace WpfApp
             }
         }
 
-        private void HighlightButton(StackPanel stackPanel, string key)
+        private void HighlightButton(string key)
         {
-            foreach (var item in stackPanel.Children)
+            foreach (var button in buttons)
             {
-                if (item is Button)
+                if (button.Content.ToString() == key)
                 {
-                    if ((item as Button).Content.ToString() == char.ToLower(key[0]).ToString())
-                    {
-                        (item as Button).Background = new SolidColorBrush(Color.FromRgb(0x76, 0xff, 0x03));
-                        break;
-                    }
+                    button.Background = new SolidColorBrush(Color.FromRgb(0x76, 0xff, 0x03));
                 }
             }
         }
 
+        private void UnhighlightButtons()
+        {
+            foreach (var button in buttons)
+            {
+                button.Background = new SolidColorBrush(Color.FromRgb(0x30, 0x30, 0x30));
+            }
+        }
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
             UnhighlightButton(SPBase, e.Key.ToString());
