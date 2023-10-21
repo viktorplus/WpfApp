32a5c089c0343bcc920d580ebf5de15bb5ca1cde
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-21 03:37:21 +0200

    ПОКА НА ЭТОМ ОСТАНОВИМСЯ ПРОДОЛЖАТЬ МОЖНО БЕСКОНЕЧНО

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,10 +16,13 @@ namespace WpfApp
         List<char> chars = new() { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', };
         List<char> chars2 = new() { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'' };
         List<char> chars3 = new() { 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/' };
-        List<char> chars4 = new() { '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=' };
+        List<char> chars4 = new() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=' };
 
         List<Button> buttons = new ();
-        string enteredText;
+        string? enteredText;
+        private int correctKeyPresses = 0;    // количество правильных нажатий
+        private int incorrectKeyPresses = 0;  // количество неправильных нажатий
+
 
         public MainWindow()
         {
@@ -28,6 +31,7 @@ namespace WpfApp
             CreateButtons(SPBase2, chars2);
             CreateButtons(SPBase3, chars3);
             CreateButtons(SPBase4, chars4);
+
         }
         //добавление кнопок в каждую StackPanel отдельно со стилем
         private void CreateButtons(StackPanel stackPanel, List<char> charsList)
@@ -61,18 +65,23 @@ namespace WpfApp
         private void tbText_KeyDown(object sender, KeyEventArgs e)
         {
             char sign;
-            string key = e.Key.ToString().ToLower();
-            if (key == "Space".ToLower())
+            string key = e.Key.ToString();
+            if (key.Length == 2 && key[0] == 'D')
+            {
+                sign = key[1]; // Удалить первый символ для цифр
+            }
+            else if (key == "Space")
             {
                 sign = ' ';
             }
+
             else
             {
-                sign = key[0];
+                sign = char.ToLower(key[0]);
             }
             // Вывод информации о значениях переменных в окно вывода
-            Debug.WriteLine("Key Pressed: " + key);
-            Debug.WriteLine("First Character in TextBox: " + tbText.Text.First());
+            //Debug.WriteLine("Key Pressed: " + key);
+            //Debug.WriteLine("First Character in TextBox: " + tbText.Text.First());
 
             if (sign == tbText.Text.First())
             {
@@ -81,29 +90,41 @@ namespace WpfApp
                 stringBuilder.Remove(0, 1);
                 tbText.Text = stringBuilder.ToString();
                 tbText.UpdateLayout();
+                correctKeyPresses++; // увеличиваем счетчик правильных нажатий
             }
             else
             {
                 HighlightButtonFalse(sign.ToString());
+                incorrectKeyPresses++; // увеличиваем счетчик неправильных нажатий
             }
+            UpdateKeyPressStatistics();
 
         }
+        private void UpdateKeyPressStatistics()
+        {
+            int totalKeyPresses = correctKeyPresses + incorrectKeyPresses;
+            double percentageCorrect = totalKeyPresses == 0 ? 100.0 : (correctKeyPresses * 100.0) / totalKeyPresses;
 
-        private void HighlightButtonOk(string key)
+            // Обновляем TextBlock для отображения статистики
+            // Создайте TextBlock с именем, например, statsTextBlock в вашем XAML и свяжите его с этим кодом.
+            statsTextBlock.Text = $"Правильно: {correctKeyPresses}, Неправильно: {incorrectKeyPresses}, " +
+                                 $"Процент правильных: {percentageCorrect:F2}%";
+        }
+        private void HighlightButtonOk(string sign)
         {
             foreach (var button in buttons)
             {
-                if (button.Content.ToString() == key)
+                if (button.Content.ToString() == sign)
                 {
                     button.Background = new SolidColorBrush(Color.FromRgb(0x76, 0xff, 0x03));
                 }
             }
         }
-        private void HighlightButtonFalse(string key)
+        private void HighlightButtonFalse(string sign)
         {
             foreach (var button in buttons)
             {
-                if (button.Content.ToString() == key)
+                if (button.Content.ToString() == sign)
                 {
                     button.Background = new SolidColorBrush(Colors.IndianRed);
                 }
@@ -119,7 +140,15 @@ namespace WpfApp
         }
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
-            UnhighlightButtons(e.Key.ToString());
+            string key;
+            string sign = e.Key.ToString();
+            if (sign.Length == 2 && sign[0] == 'D')
+            {
+                key = sign[1].ToString(); // Удалить первый символ для цифр
+            }
+            else key = sign[0].ToString();
+
+            UnhighlightButtons(key);
         }
 
         private void UnhighlightButtons(string key)
