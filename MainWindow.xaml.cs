859f766ed3016cfabadf4d19023069982ce96726
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-21 02:05:53 +0200

    v1.4 STABLE

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -12,12 +13,13 @@ namespace WpfApp
 {
     public partial class MainWindow : Window
     {
-        List<char> chars = new List<char>() { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', };
-        List<char> chars2 = new List<char>() { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'' };
-        List<char> chars3 = new List<char>() { 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/' };
-        List<char> chars4 = new List<char>() { '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=' };
+        List<char> chars = new() { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', };
+        List<char> chars2 = new() { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'' };
+        List<char> chars3 = new() { 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/' };
+        List<char> chars4 = new() { '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=' };
 
-        List<Button> buttons = new List<Button>();
+        List<Button> buttons = new ();
+        string enteredText;
 
         public MainWindow()
         {
@@ -26,84 +28,84 @@ namespace WpfApp
             CreateButtons(SPBase2, chars2);
             CreateButtons(SPBase3, chars3);
             CreateButtons(SPBase4, chars4);
-
-            foreach (var item in SPBase.Children)
-            {
-                if (item is Button button)
-                {
-                    buttons.Add(button);
-                }
-            }
-            foreach (var item in SPBase2.Children)
-            {
-                if (item is Button button)
-                {
-                    buttons.Add(button);
-                }
-            }
-            foreach (var item in SPBase3.Children)
-            {
-                if (item is Button button)
-                {
-                    buttons.Add(button);
-                }
-            }
-            foreach (var item in SPBase4.Children)
-            {
-                if (item is Button button)
-                {
-                    buttons.Add(button);
-                }
-            }
         }
-
+        //добавление кнопок в каждую StackPanel отдельно со стилем
         private void CreateButtons(StackPanel stackPanel, List<char> charsList)
         {
             stackPanel.Orientation = Orientation.Horizontal;
             stackPanel.HorizontalAlignment = HorizontalAlignment.Stretch;
+
             for (int i = 0; i < charsList.Count; i++)
             {
-                var b = new Button() { Content = charsList[i].ToString(), Width = 50, Height = 50 };
-                b.Style = (Style)FindResource("MaterialDesignPaperButton");
+                var b = new Button
+                {
+                    Content = charsList[i].ToString(),
+                    Width = 50,
+                    Height = 50,
+                    Style = (Style)FindResource("MaterialDesignPaperButton")
+                };
                 stackPanel.Children.Add(b);
+
+                // Добавить кнопку в общий список всех кнопок buttons
+                buttons.Add(b);
             }
         }
 
+
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            enteredText = tbText.Text.ToLower();
+            tbText.Text = enteredText;
         }
 
         private void tbText_KeyDown(object sender, KeyEventArgs e)
         {
             char sign;
-            string key = e.Key.ToString();
-            if (key == "Space")
+            string key = e.Key.ToString().ToLower();
+            if (key == "Space".ToLower())
             {
                 sign = ' ';
             }
             else
             {
-                sign = char.ToLower(key[0]);
+                sign = key[0];
             }
+            // Вывод информации о значениях переменных в окно вывода
+            Debug.WriteLine("Key Pressed: " + key);
+            Debug.WriteLine("First Character in TextBox: " + tbText.Text.First());
 
             if (sign == tbText.Text.First())
             {
-                HighlightButton(sign.ToString());
-                StringBuilder stringBuilder = new StringBuilder(tbText.Text);
+                HighlightButtonOk(sign.ToString());
+                StringBuilder stringBuilder = new (tbText.Text);
                 stringBuilder.Remove(0, 1);
                 tbText.Text = stringBuilder.ToString();
                 tbText.UpdateLayout();
             }
+            else
+            {
+                HighlightButtonFalse(sign.ToString());
+            }
+
         }
 
-        private void HighlightButton(string key)
+        private void HighlightButtonOk(string key)
         {
             foreach (var button in buttons)
             {
                 if (button.Content.ToString() == key)
                 {
                     button.Background = new SolidColorBrush(Color.FromRgb(0x76, 0xff, 0x03));
+                }
+            }
+        }
+        private void HighlightButtonFalse(string key)
+        {
+            foreach (var button in buttons)
+            {
+                if (button.Content.ToString() == key)
+                {
+                    button.Background = new SolidColorBrush(Colors.IndianRed);
                 }
             }
         }
@@ -117,25 +119,20 @@ namespace WpfApp
         }
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
-            UnhighlightButton(SPBase, e.Key.ToString());
-            UnhighlightButton(SPBase2, e.Key.ToString());
-            UnhighlightButton(SPBase3, e.Key.ToString());
-            UnhighlightButton(SPBase4, e.Key.ToString());
+            UnhighlightButtons(e.Key.ToString());
         }
 
-        private void UnhighlightButton(StackPanel stackPanel, string key)
+        private void UnhighlightButtons(string key)
         {
-            foreach (var item in stackPanel.Children)
+            foreach (var button in buttons)
             {
-                if (item is Button)
+                if (button.Content.ToString() == char.ToLower(key[0]).ToString())
                 {
-                    if ((item as Button).Content.ToString() == char.ToLower(key[0]).ToString())
-                    {
-                        (item as Button).Background = new SolidColorBrush(Color.FromRgb(0x30, 0x30, 0xff));
-                        (item as Button).UpdateLayout();
-                    }
+                    button.Background = new SolidColorBrush(Color.FromRgb(0x30, 0x30, 0x30));
+                    button.UpdateLayout();
                 }
             }
         }
+
     }
 }
