b3f73ae5a5a5794d3a7d348c15f50bb87dd7616e
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-31 23:58:40 +0100

    1

---
+++ Calculator.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text;
+
+namespace WpfApp
+{
+    public class Calculator
+    {
+        private StringBuilder currentNumber;
+        private StringBuilder history;
+        private string lastOperation;
+        private double result;
+
+        public Calculator()
+        {
+            currentNumber = new StringBuilder();
+            history = new StringBuilder();
+            lastOperation = "";
+            result = 0;
+        }
+
+        public void AppendDigit(string digit)
+        {
+            if (digit == "0" && currentNumber.ToString() == "0")
+            {
+                // Не добавляем ведущий ноль
+                return;
+            }
+            if (digit == "." && currentNumber.ToString().Contains("."))
+            {
+                // Не добавляем вторую точку
+                return;
+            }
+            currentNumber.Append(digit);
+        }
+
+        public void SetOperation(string operation)
+        {
+            if (currentNumber.Length > 0)
+            {
+                if (lastOperation != "")
+                {
+                    Calculate();
+                }
+                lastOperation = operation;
+                result = double.Parse(currentNumber.ToString());
+                history.Append(currentNumber);
+                history.Append(" ");
+                history.Append(operation);
+                history.Append(" ");
+                currentNumber.Clear();
+            }
+        }
+
+        public void Calculate()
+        {
+            if (currentNumber.Length > 0)
+            {
+                double secondOperand = double.Parse(currentNumber.ToString());
+                switch (lastOperation)
+                {
+                    case "+":
+                        result += secondOperand;
+                        break;
+                    case "-":
+                        result -= secondOperand;
+                        break;
+                    case "*":
+                        result *= secondOperand;
+                        break;
+                    case "/":
+                        if (secondOperand != 0)
+                            result /= secondOperand;
+                        else
+                            result = double.NaN; // Обработка деления на ноль
+                        break;
+                }
+                currentNumber.Clear();
+                lastOperation = "";
+                history.Clear();
+            }
+        }
+
+        public void AppendDecimalPoint()
+        {
+            if (!currentNumber.ToString().Contains("."))
+            {
+                currentNumber.Append(".");
+            }
+        }
+
+        public void Clear()
+        {
+            currentNumber.Clear();
+        }
+
+        public void ClearAll()
+        {
+            currentNumber.Clear();
+            history.Clear();
+            lastOperation = "";
+            result = 0;
+        }
+
+        public void RemoveLastDigit()
+        {
+            if (currentNumber.Length > 0)
+            {
+                currentNumber.Remove(currentNumber.Length - 1, 1);
+            }
+        }
+
+        public string GetHistory()
+        {
+            return history.ToString();
+        }
+
+        public string GetCurrentNumber()
+        {
+            if (currentNumber.Length == 0)
+            {
+                return result.ToString();
+            }
+            return currentNumber.ToString();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Calculator calculator;
+
         public MainWindow()
         {
             InitializeComponent();
+            calculator = new Calculator();
+        }
+
+        private void Digit_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string digit = button.Content.ToString();
+            calculator.AppendDigit(digit);
+            UpdateDisplay();
+        }
+
+        private void Operation_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string operation = button.Content.ToString();
+            calculator.SetOperation(operation);
+            UpdateDisplay();
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Calculate();
+            UpdateDisplay();
+        }
+
+        private void DecimalPoint_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendDecimalPoint();
+            UpdateDisplay();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Clear();
+            UpdateDisplay();
+        }
+
+        private void ClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.ClearAll();
+            UpdateDisplay();
+        }
+
+        private void RemoveLastDigit_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.RemoveLastDigit();
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            historyTextBox.Text = calculator.GetHistory();
+            currentNumberTextBox.Text = calculator.GetCurrentNumber();
         }
     }
 }
