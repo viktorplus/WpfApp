d857925b113cb0c7bbd27b13516058563662b0bb
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-11-01 21:59:33 +0100

    готово

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (UsernameTextBox.Text == "user" && PasswordBox.Password == "password")
+            {
+
+            }
+            else
+            {
+                ErrorMessageTextBlock.Text = "Ошибка входа. Пожалуйста, проверьте имя пользователя и пароль.";
+                ErrorMessageTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            UsernameTextBox.Text = string.Empty;
+            PasswordBox.Password = string.Empty;
+            ErrorMessageTextBlock.Visibility = Visibility.Collapsed;
         }
     }
 }
