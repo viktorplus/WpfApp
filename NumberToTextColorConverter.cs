8d56c05fef5948c7f02b0675462e90679aeec695
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-11-08 04:17:25 +0100

    Foreground

---
+++ NumberToTextColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp
+{
+    public class NumberToTextColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int number = (int)value;
+
+            if (number == 0)
+            {
+                return Brushes.White; // Белый цвет текста для плиток с нулевым значением
+            }
+            else
+            {
+                return Brushes.Black; // Черный цвет текста для непустых плиток
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
