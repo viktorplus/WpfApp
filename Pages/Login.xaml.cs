5ba44f5f829735159415a26a22830d5d451ad013
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-11-09 18:02:07 +0100

    9+

--- Pages/Login.xaml.cs
+++ Pages/Login.xaml.cs
@@ -5,32 +5,13 @@ using WpfApp.Navigator;
 
 namespace WpfApp.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Login.xaml
-    /// </summary>
     public partial class Login : UserControl
     {
         Frame MainFrame;
         public Login(Frame MainFrame)
         {
             InitializeComponent();
-            this.MainFrame = MainFrame;
 
-        }
-        public void Navigate(UserControl nextPage)
-        {
-            this.Content = nextPage;
-        }
-
-        public void Navigate(UserControl nextPage, object state)
-        {
-            this.Content = nextPage;
-            INavigator? s = nextPage as INavigator;
-
-            if (s != null)
-                s.UtilizeState(state);
-            else
-                throw new ArgumentException("NextPage is not INavigator! " + nextPage.Name.ToString());
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
@@ -38,19 +19,19 @@ namespace WpfApp.Pages
             string login = LoginTextBox.Text;
             string password = PasswordBox.Password;
 
-            if (MainPage.userList.ValidateUser(login, password))
-            {
-                MainPage.CurrentUser = MainPage.userList.GetUserByLogin(login);
+            //if (MainPage.userList.ValidateUser(login, password))
+            //{
+                //MainPage.CurrentUser = MainPage.userList.GetUserByLogin(login);
 
                 //MainFrame.Content = new MainAdmin(MainFrame);
                 NavigatorObject.Switch(new MainAdmin());
 
 
-            }
-            else
-            {
-                MessageBox.Show("Неправильный логин или пароль. Попробуйте еще раз.");
-            }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Неправильный логин или пароль. Попробуйте еще раз.");
+            //}
         }
 
 
