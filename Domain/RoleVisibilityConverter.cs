b98deda19c77dc66fdaa9a715482b1c0cea5f911
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-11-10 01:51:07 +0100

    13

---
+++ Domain/RoleVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+using System;
+
+namespace WpfApp.Domain
+{
+    public class RoleVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Проверяем, является ли роль "Студент"
+            return (value as List<User.UserRole>)?.Contains(User.UserRole.Student) == true ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
